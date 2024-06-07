1c45292e581cf1f42e19cbde702aa6f84b89380c
SCampagnese <salvatore.campagnese@gmail.com>
2024-06-07 15:54:34 +0200

    page invite

--- app/app/invite/page.tsx
+++ app/app/invite/page.tsx
@@ -1,17 +1,22 @@
 "use client";
-
+import { useState, useEffect } from "react";
 function InvitePage() {
+    const [location, setLocation] = useState<string>("");
+    // useeffect that get the current window location
+    useEffect(() => {
+        setLocation(window?.location?.href);
+    }, []);
     return (
         <div className="flex min-h-screen flex-col bg-[url('/bg.svg')] bg-cover bg-no-repeat h-full justify-center items-center sm:px-[150px] sm:overflow-hidden">
             <p className="text-[34px] sm:text-[56px] text-center sm:text-left" style={{
                 lineHeight: '1.2'
             }}>
-                Do you already have Babol? <a className="text-[#A08CF3]" style={{ fontFamily: 'satoshiBold' }} href={window?.location?.href}>Click Here</a>
+                Do you already have Babol? <a className="text-[#A08CF3]" style={{ fontFamily: 'satoshiBold' }} href={location}>Click Here</a>
             </p>
             <p className="text-[20px] sm:text-[24px] text-center sm:text-left" style={{
                 lineHeight: '1.2'
             }}>
-                or download it <a className="text-[#A08CF3]" style={{ fontFamily: 'satoshiBold' }} href={window?.location?.href}>HERE</a>
+                or download it <a className="text-[#A08CF3]" style={{ fontFamily: 'satoshiBold' }} href={location}>HERE</a>
             </p>
         </div>
     );
