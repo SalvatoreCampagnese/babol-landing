2b7061d54ad3187e1ee2596201ff3f96ae1ed200
SCampagnese <salvatore.campagnese@gmail.com>
2024-06-18 11:31:42 +0200

    fix varie

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -9,7 +9,7 @@ export default function ThankYou() {
       <div className="box overflow-visible w-screen">
         <div className="circle"></div>
       </div>
-      <main className="flex min-h-screen flex-col bg-[url('/bg2.svg')] bg-cover bg-no-repeat h-[100vh] justify-center
+      <main className="flex min-h-screen flex-col bg-[url('/bg-mobile.svg')] sm:bg-[url('/bg2.svg')] bg-cover bg-no-repeat h-[100vh] justify-center
         sm:px-[0px]
         md:px-[10px]
         md:overflow-hidden">
