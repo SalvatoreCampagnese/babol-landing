f0f0ddf4b41b006ca3605dbe6ea479e6cddfcf69
SCampagnese <salvatore.campagnese@gmail.com>
2024-07-15 15:36:53 +0200

    test

---
+++ app/v2/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import Script from "next/script";
+import IubendaScripts from "../components/IubendaScripts";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Babol App",
+  description: "Organize your events in a new way! Everything in one place!",
+};
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js" />
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/ScrollTrigger.min.js" />
+      <Script src="https://unpkg.com/split-type" />
+      <Script src="https://cdn.jsdelivr.net/gh/studio-freight/lenis@1.0.19/bundled/lenis.min.js" />
+      
+      <Script src="/animation.js" />
+      </head>
+        <IubendaScripts />
+      <body>{children}</body>
+    </html>
+  );
+}
