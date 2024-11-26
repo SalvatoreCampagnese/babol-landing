69c7eac704116ad136ae578912f2966b0dcd835b
salvatore campagnese <salvatore.campagnese@gmail.com>
2024-11-26 11:54:28 +0100

    test

--- app/app/invite/page.tsx
+++ app/app/invite/page.tsx
@@ -17,7 +17,7 @@ function InvitePage() {
     const fetchBabolByInvite = async () => {
       const userData = await supabase.auth.getSession();
       if (!userData?.data?.session) {
-        //router.replace("https://www.babol.app/app/login");
+        router.replace("https://www.babol.app/404?c=" + invite_code);
         return;
       }
       const { data: babol, error: errorBabol } = await supabase
@@ -34,68 +34,7 @@ function InvitePage() {
     };
     fetchBabolByInvite();
   }, [invite_code]);
-  return (
-    <>
-      <div className="fixed w-full h-fit rounded-md flex flex-col bg-white text-black p-4 md:p-16 text-center overflow-y-scroll relative text-black">
-        <h1 className="text-3xl font-satoshiBold text-black bold">
-          Join Babol
-        </h1>
-        If you are not automatically redirect, click the link below
-        <br />
-        <Link
-          href={"babol://app/invite?c=" + invite_code}
-          className="text-xl font-satoshiBold font-bold"
-        >
-          OPEN INVITE
-        </Link>
-        <div className="flex flex-col md:flex-row md:gap-16">
-          <div className="mt-6 flex flex-col md:gap-2">
-            <h3 className="text-black font-satoshiBold text-xl md:text-3xl">
-              Download the iOS app
-            </h3>
-            <span className="text-black">
-              Scan the QR code or{" "}
-              <Link
-                href={"https://apps.apple.com/us/app/babol/id6478817773"}
-                target="_blank"
-              >
-                click here
-              </Link>
-            </span>
-            <Image src={iosQrCode} alt="qr-code" width={1000} height={1000} />
-          </div>
-          <div className="flex flex-col md:flex-row md:gap-16">
-            <div className="mt-6 flex flex-col md:gap-2">
-              <h3 className="text-black font-satoshiBold text-xl md:text-3xl">
-                Download the android app
-              </h3>
-              <span className="text-black">
-                Scan the QR code or{" "}
-                <Link
-                  href={
-                    "https://play.google.com/store/apps/details?id=com.babol.app&pli=1"
-                  }
-                  target="_blank"
-                >
-                  click here
-                </Link>
-              </span>
-              <Image
-                src={androidQrCode}
-                alt="qr-code"
-                width={1000}
-                height={1000}
-              />
-            </div>
-          </div>
-        </div>
-        <span className="text-black mt-4">
-          For all other info go to{" "}
-          <Link href={"www.babol.app"}>www.babol.app</Link>
-        </span>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default InvitePage;
